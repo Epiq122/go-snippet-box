b9db3803d0aa586856010b447a84cc38132645e1
Robert Gleason <l2ob122@gmail.com>
2024-03-11 16:47:36 -0700

    Update view template for snippet display

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -54,7 +54,7 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	files := []string{
 		"./ui/html/base.tmpl.html",
 		"./ui/html/partials/nav.tmpl.html",
-		"./ui/html/pages/home.tmpl.html",
+		"./ui/html/pages/view.tmpl.html",
 	}
 
 	ts, err := template.ParseFiles(files...)
