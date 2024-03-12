75bff7aff364d6da2941d660f3aa368e3872d5fd
Robert Gleason <l2ob122@gmail.com>
2024-03-11 22:16:41 -0700

    Refactor template rendering and caching

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,10 +1,41 @@
 package main
 
 import (
+	"html/template"
+	"path/filepath"
+
 	"snippetbox.robertgleason.ca/internal/models"
 )
 
 type templateData struct {
 	Snippet  models.Snippet
 	Snippets []*models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl.html")
+	if err != nil {
+		return nil, err
+	}
+
+	// Loop through the page filepaths one-by-one.
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		files := []string{
+			"./ui/html/base.tmpl.html",
+			"./ui/html/partials/nav.tmpl.html",
+			page,
+		}
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+
+	return cache, nil
 }
